7a5352c53d6dfc7a2f3ebf810c44915da1205888
cristianocsl <cslcristiano@gmail.com>
2022-03-10 19:15:08 -0300

    Requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=5)
+        time.sleep(1)
+    except requests.ReadTimeout:
+        return None
+    if response.status_code != 200:
+        return None
+    return response.text
 
 
 # Requisito 2
